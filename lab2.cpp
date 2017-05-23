1868846af60941b2c6dcca60beebef6adf609a83
cathyatseneca <cathyatseneca@gmail.com>
2017-05-23 13:04:44 -0400

    lab 2 updated

--- lab2.cpp
+++ lab2.cpp
@@ -1,3 +1,14 @@
+//to compile: g++ lab2.cpp timer.cpp -std=c++0x
+#include "timer.h"
 unsigned int factorial (unsigned int n);
 double power (double base, unsigned int n);
 unsigned int fibonacci (unsigned int n);
+
+
+int main(void){
+	Timer T;
+	T.start();
+	fibonacci(35);
+	T.stop();
+	cout << "it took: " << T.currtime() << " seconds"<<endl;
+}	
