1f9b11ba563f0440e395955b87263f66888bded3
cathyatseneca <cathyatseneca@gmail.com>
2017-05-30 11:56:08 -0400

    updated dlist

--- dlist.h
+++ dlist.h
@@ -1,5 +1,6 @@
 //templated doubly linked list
-
+#include <iostream>
+using namespace std;
 template <typename T>
 class DList{
 	struct Node{
@@ -15,14 +16,131 @@ class DList{
 	Node* front_;
 	Node* back_;
 public:
-	class iterator{
+	class const_iterator{
+		friend class DList;
+	protected:
+		Node* curr_;
+		const_iterator(Node* n){
+			curr_=n;
+		}
+	public:
+		const_iterator(){
+			curr_=nullptr;
+		}
+		//++x
+		const_iterator operator++(){
+			if(curr_){
+				curr_=curr_->next_;
+			}
+			return *this;
+		}
+		//x++
+		const_iterator operator++(int){
+			const_iterator old=*this;
+			if(curr_){
+				curr_=curr_->next_;
+			}
+			return old;			
+		}
+		const_iterator operator--(){
+			if(curr_){
+				curr_=curr_->prev_;
+			}
+			return *this;
 
+		}
+		const_iterator operator--(int){
+			const_iterator old=*this;
+			if(curr_){
+				curr_=curr_->prev_;
+			}
+			return old;			
+		}
+		T operator*()const{
+			return curr_->data_;
+		}
+		bool operator==(const_iterator other) const{
+			return curr_==other.curr_;
+		}
+		bool operator!=(const_iterator other) const{
+			return curr_!=other.curr_;
+		}
 	};
+	class iterator:const_iterator{
+		friend class DList;
+		iterator(Node* n):const_iterator(n){
+
+		}
+	public:
+		iterator():const_iterator(){
+		}
+		//++x
+		iterator operator++(){
+			if(this->curr_){
+				this->curr_=this->curr_->next_;
+			}
+			return *this;
+		}
+		//x++
+		iterator operator++(int){
+			iterator old=*this;
+			if(this->curr_){
+				this->curr_=this->curr_->next_;
+			}
+			return old;			
+		}
+		iterator operator--(){
+			if(this->curr_){
+				this->curr_=this->curr_->prev_;
+			}
+			return *this;
+
+		}
+		iterator operator--(int){
+			iterator old=*this;
+			if(this->curr_){
+				this->curr_=this->curr_->prev_;
+			}
+			return old;			
+		}
+		T& operator*(){
+			return this->curr_->data_;
+		}
+		T operator*()const{
+			return this->curr_->data_;
+		}
+		bool operator==(iterator other) const{
+			return this->curr_==other.curr_;
+		}
+		bool operator!=(iterator other) const{
+			return this->curr_!=other.curr_;
+		}
+	};
+	//returns an iterator to first piece of data
+	//in DList
+	iterator begin(){
+		return iterator(front_);
+	}
+	//returns an iterator to the thing that
+	//FOLLOWS the last piece of data in DList;
+	iterator end(){
+		return iterator(nullptr);
+	}
+	//returns an iterator to first piece of data
+	//in DList
+	const_iterator begin() const{
+		return iterator(front_);
+	}
+	//returns an iterator to the thing that
+	//FOLLOWS the last piece of data in DList;
+	const_iterator end() const{
+		return iterator(nullptr);
+	}
 	DList(){
 		front_=back_=nullptr;
 	}
 	void push_front(const T& data){
-		if(its an empty list){
+		if(front_==nullptr){
 		//make a node
 			Node* nn=new Node(data);
 			//make front point to the node
@@ -43,7 +161,7 @@ public:
 
 	}
 	void pop_front(){
-		if(the list is not empty){
+		if(front_){
 			Node* rm = front_;
 			front_=front_->next_;
 			delete rm;
@@ -57,5 +175,11 @@ public:
 		}
 	}
 	void pop_back();
-	void print() const;
+	void print() const{
+		Node* curr=front_;
+		while(curr!=nullptr){
+			cout << curr->data_ << endl;
+			curr=curr->next_;
+		}	
+	}
 };
